9e4d92b406f243c86d6cc228b7dc75db8fcc982e
wilas0o0o <03mcu30@gmail.com>
2022-01-06 08:04:18 +0000

    Finish Comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,2 +1,27 @@
 class BookCommentsController < ApplicationController
+
+  def create
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.user_id = current_user.id
+    if @book_comment.save
+      redirect_to request.referer, notice: "You have created comment successfully."
+    else
+      render 'books/show'
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:book_id])
+    @book_comment = @book.book_comments.find(params[:id])
+    @book_comment.destroy
+    redirect_to request.referer, notice: "Ypu have destroyed comment successfully."
+  end
+  
+  private
+  
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
 end
--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -1,2 +1,6 @@
 class BookComment < ApplicationRecord
+  belongs_to :user
+  belongs_to :book
+  
+  validates :comment, presence: true, length: { maximum: 50 }
 end
