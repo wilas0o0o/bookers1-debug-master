74e3857cd5efdf2105ac391385503faea8a95f6d
wilas0o0o <03mcu30@gmail.com>
2022-01-27 05:44:29 +0000

    [Add]GuestUser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,22 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :about]
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to user_path(user), notice: "GuestUserでログインしました"
+  end
+  
   protected
-  def after_sign_in_path_for(resource)
-    user_path(current_user)
-  end
-
-  def after_sign_out_path_for(resource)
-    root_path
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-  end
+    def after_sign_in_path_for(resource)
+      user_path(current_user)
+    end
+  
+    def after_sign_out_path_for(resource)
+      root_path
+    end
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    end
 end
